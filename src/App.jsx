599cf6f4facc0c3a24366284ca028c6fa84f4876
rajabhishek5041 <rajabhishek5041@gmail.com>
2024-10-25 21:22:32 +0530

    Four Corner Network

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-green-600'>
+        <h1>Abhishek kumar singh</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
